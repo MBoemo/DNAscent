d4853a01f1c85f105a6f8b2c4db901ddb6ef579e
Ubuntu <oxpath@oxpathUbuntuServer1604LTS.pts205ev0nqujm1rdxb2v52wme.ax.internal.cloudapp.net>
2017-03-02 11:54:59 +0000

    fix for importing fixed position training data

--- Osiris/data_IO.py
+++ Osiris/data_IO.py
@@ -176,8 +176,9 @@ def export_poreModel(emissions, outputFilename):
 	f.close()
 
 
-def calculate_normalisedEvents(kmer, fast5Files, poreModelFile, progress):
-#	Uses a 5mer model to calculate the shift and scale pore-specific parameters for each individual read in a list of fast5 files
+def parallel_calculate_normalisedEvents(kmer, fast5Files, poreModelFile, progress):
+#	For hairpin training data - the large number of kmers makes it best to use parallel processing.  Uses a 5mer model to
+#	calculate the shift and scale pore-specific parameters for each individual read in a list of fast5 files
 #	ARGUMENTS
 #       ---------
 #	- kmer: redundant 6mer to identify the reads we're normalising
@@ -246,6 +247,71 @@ def calculate_normalisedEvents(kmer, fast5Files, poreModelFile, progress):
 	return (kmer, allNormalisedReads)
 
 
+def serial_calculate_normalisedEvents(fast5Files, poreModelFile):
+#	For fixed position training data - small enough to be done in serial.  Uses a 5mer model to calculate the shift and scale
+#	pore-specific parameters for each individual read in a list of fast5 files
+#	ARGUMENTS
+#       ---------
+#	- fast5Files: list of fast5 files whose events should be normalised
+#	  type: list of strings
+#	- poreModelFile: path to a pore model file.  This should be a 5mer model in the ONT format
+#	  type: string
+#	OUTPUTS
+#       -------
+#	- allNormalisedReads: a list, where each member is itself a list of events that have been normalised to the pore model
+#	  type: list
+
+	#open the 5mer model and make a map from the 5mer (string) to [mean,std] (list)
+	kmer2MeanStd = import_poreModel(poreModelFile)
+
+	#now iterate through all the relevant fast5 files so we only need to open the model file once
+	allNormalisedReads = []
+	for f5File in fast5Files:
+
+		#use 5mer model to calculate shift, scale, drift, and var to normalise events for the pore
+		f = h5py.File(f5File,'r')
+		path = '/Analyses/Basecall_1D_000/BaseCalled_template/Events'
+		Events = f[path]
+		A = np.zeros((2,2))
+		b = np.zeros((2,1))
+		for event in Events:
+			 if float(event[7]) > 0.30: #if there's a high probability (>30%) that the 5mer model called by Metrichor was the correct one
+				model_5mer = event[4]
+				event_mean = float(event[0])
+				model_mean = kmer2MeanStd[model_5mer][0]
+				model_std = kmer2MeanStd[model_5mer][1]
+				
+				#update matrix A
+				A[0,0] += 1/(model_std**2)
+				A[1,0] += 1/(model_std**2)*model_mean
+				A[1,1] += 1/(model_std**2)*model_mean**2
+
+				#update vector b
+				b[0] += 1/(model_std**2)*event_mean
+				b[1] += 1/(model_std**2)*event_mean*model_mean
+
+		#use symmetry of A
+		A[0,1] = A[1,0]
+
+		#solve Ax = b to find shift and scale
+		x = np.linalg.solve(A,b)
+		shift = x[0][0]
+		scale = x[1][0]
+
+		#go through the same events as before and normalise them to the pore model using scale and shift
+		normalisedEvents = []
+		for event in Events:
+			if float(event[7]) > 0.30: #if there's a high probability (>30%) that the 5mer model called by Metrichor was the correct one
+				event_mean = float(event[0])
+				normalisedEvents.append( event_mean/scale - shift)
+
+		allNormalisedReads.append(normalisedEvents)
+
+		f.close()
+	
+	return allNormalisedReads
+
+
 def import_FixedPosTrainingData(bamFile, poreModelFile):
 #	Used to import training data from reads that have an analogue in a fixed context.
 #	Creates a map from kmer (string) to a list of lists, where each list is comprised of events from a read
@@ -279,7 +345,7 @@ def import_FixedPosTrainingData(bamFile, poreModelFile):
 	f.close()
 
 	#hand this list of fast5 files to calculate_normalisedEvents which will normalise them for shift and scale
-	normalisedReads = calculate_normalisedEvents(fast5files, poreModelFile)
+	normalisedReads = serial_calculate_normalisedEvents(fast5files, poreModelFile)
 
 	return normalisedReads
 
@@ -355,7 +421,7 @@ def import_HairpinTrainingData(reference, bamFile, poreModelFile, redundant_A_Lo
 	del kmer2Files
 
 	#do the parallel processing, where a kmer (and associated reads) is given to each core.  use the maximum number of cores available
-	normalisedReadsTuples = Parallel(n_jobs = multiprocessing.cpu_count())(delayed(calculate_normalisedEvents)(key, filteredKmer2Files[key], poreModelFile, (i,len(filteredKmer2Files))) for i, key in enumerate(filteredKmer2Files))
+	normalisedReadsTuples = Parallel(n_jobs = multiprocessing.cpu_count())(delayed(parallel_calculate_normalisedEvents)(key, filteredKmer2Files[key], poreModelFile, (i,len(filteredKmer2Files))) for i, key in enumerate(filteredKmer2Files))
 
 	#reshape the list of tuples from parallel processing into a dictionary
 	kmer2normalisedReads = {}
